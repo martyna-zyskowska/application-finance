7be88d468aebec2974e96daae75937e193d4add4
Martyna <39814780+martyna-zyskowska@users.noreply.github.com>
2018-07-13 16:12:46 +1000

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+        <div style="clear:both"></div>
+
+        <footer>
+            <span id="footerTitle">&copy; 2018 Martyna Zyskowska. All rights reserved.</span>
+        </footer>
+
+    </body>
+</html>
