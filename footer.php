a33b9df258f82e9dcd2b327423299f95d20bc80a
Martyna <39814780+martyna-zyskowska@users.noreply.github.com>
2018-09-28 15:28:48 +1000

    Delete footer.php

--- footer.php
+++
@@ -1,8 +0,0 @@
-        <div style="clear:both"></div>
-
-        <footer>
-            <span id="footerTitle">&copy; 2018 Martyna Zyskowska. All rights reserved.</span>
-        </footer>
-
-    </body>
-</html>
